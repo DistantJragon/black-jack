12ba82b8479865e6d9aa825a4bc1e5db08fb4fe1
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2020-10-02 21:35:27 -0400

    setup and skeleton, start work on game

--- blackjack.py
+++ blackjack.py
@@ -35,29 +35,42 @@ class Card:
 
         self.suitName = suitList[suitNumber]
         self.cardName = cardList[cardNumber]
-        self.cardFullName = '{} of {}'.format(self.cardName, self.suitName)
+        self.textureName = ''
 
         self.cardID = (deckNumber * 52) + (suitNumber * 13) + cardNumber
         self.worth = get_card_value_from_card_number(cardNumber)
         self.isFaceDown = False
     def softify(self):
-        if self.suitNumber != 0: raise Exception('Tried to make a card that wasn\'t an ace soft')
+        if self.cardNumber != 0: raise Exception('Tried to make a card that wasn\'t an ace soft')
         else:
             self.worth = 1
+    def full_name(self):
+        if self.cardNumber == 0:
+            if self.worth == 11: self.textureName = ' (Soft)'
+            elif self.worth == 1: self.textureName = ' (Hard)' 
+        tempCardFullName = '{} of {}{}'.format(self.cardName, self.suitName, self.textureName)
+        return tempCardFullName
 
 class Player:
     def __init__(self, isDealer = False):
-        self.hand = []
-        self.isSoft = False
-        self.worth = 0
         self.isDealer = isDealer
+
+        self.hand = []
+        self.handIsSoft = False
+        self.handWorth = 0
+
+        self.choice = ''
+        self.canChooseNow = True
+        self.hasAbilityToChoose = True
+        if self.isDealer:
+            self.hasAbilityToChoose = False
     def check_soft(self):
         tempSoft = False
         for card in self.hand:
             if card.worth == 11: 
                 tempSoft = True
                 break
-        self.isSoft = tempSoft
+        self.handIsSoft = tempSoft
     def check_worth_of_hand(self):
         totalWorth = 0
         for card in self.hand: totalWorth += card.worth
@@ -110,7 +123,41 @@ random.shuffle(shuffledDeck)
 for i in range(2):
     for player in players:
         tempCard = get_card_from_id(shuffledDeck[-1])
-        if player.isDealer and i == 0:
-            tempCard.isFaceDown = True
         player.hand.append(tempCard)
         shuffledDeck.pop()
+players[-1].hand[0].isFaceDown = True
+
+gameTime = True
+roundNumber = 0
+while gameTime:
+    for player in players:
+        player.check_soft()
+        player.check_worth_of_hand()
+
+        if player.isDealer == False:
+            print('Dealer\'s Hand:')
+            for card in players[-1].hand:
+                if card.isFaceDown == True: print('*Face Down*')
+                else: print(card.full_name())
+            print('')
+
+            print('Your Hand:')
+            for card in player.hand: print(card.full_name())
+            print('')
+
+        while player.handWorth > 21 and player.handIsSoft:
+            for card in player.hand:
+                if card.worth == 11:
+                    card.worth = 1
+                    if player.isDealer == False: print('Your first soft ace must harden to keep you in the game')
+                    player.check_soft()
+                    player.check_worth_of_hand()
+
+                    if player.isDealer == False:
+                        print('Your Hand:')
+                        for card in player.hand: print(card.full_name())
+                        print('')
+        if player.handWorth > 21 and player.handIsSoft == False:
+            if player.isDealer == False: print('Your hand exceeds 21 and you don\'t have a soft ace. You are bust')
+            player.hasAbilityToChoose = False
+    roundNumber += 1
